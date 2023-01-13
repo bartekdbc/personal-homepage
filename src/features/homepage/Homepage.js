62f80faf4239307d81a49806da46b52e0afaf1bf
Bartek Debiec <bartekdbc@gmail.com>
2023-01-13 15:44:31 +0800

    Homepage component created, styles implemented to it

---
+++ src/features/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import { Container } from "./Container";
+import Header from "./Header";
+
+function Homepage() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Homepage;
