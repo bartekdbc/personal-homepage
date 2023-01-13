f1b380ab132a58f1e2f050382432a0ed4088f95d
Bartek Debiec <bartekdbc@gmail.com>
2023-01-13 17:27:30 +0800

    colors in theme updated

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,10 @@
 export const theme = {
   color: {
+    black: "#252525",
+    white: "#FFFFFF",
     whiteLilac: "#FBFBFE",
+    slateGray: "#6E7E91",
+    scienceBlue: "#0366D6",
   },
   breakpoint: {},
 };
