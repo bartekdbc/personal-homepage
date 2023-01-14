02aa65acdc2133521c6fc60c0771ca568f899538
Bartek Debiec <bartekdbc@gmail.com>
2023-01-14 14:54:47 +0800

    new colors and breakpoints in theme

--- src/theme.js
+++ src/theme.js
@@ -4,11 +4,13 @@ export const theme = {
     white: "#FFFFFF",
     whiteLilac: "#FBFBFE",
     slateGray: "#6E7E91",
+    underlineGray: "rgba(209, 213, 218, 0.3)",
     scienceBlue: "#0366D6",
+    cornflowerBlue: "#0366D6",
   },
   breakpoint: {
-    small: "467px",
-    mobile: "767px",
+    small: "767px",
     medium: "991px",
+    large: "1199px",
   },
 };
