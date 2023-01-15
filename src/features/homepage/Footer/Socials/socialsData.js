7d687c260f73ea08cc956c3d61203be23411a3b4
Bartek Debiec <bartekdbc@gmail.com>
2023-01-15 14:51:58 +0800

    socialsData array added

---
+++ src/features/homepage/Footer/Socials/socialsData.js
@@ -0,0 +1,21 @@
+import { ReactComponent as GitHubIcon } from "./images/github.svg";
+import { ReactComponent as LinkedInIcon } from "./images/linkedin.svg";
+import { ReactComponent as FacebookIcon } from "./images/facebook.svg";
+
+export const socialsData = [
+  {
+    name: "GitHub",
+    address: "https://github.com/bartekdbc",
+    icon: GitHubIcon,
+  },
+  {
+    name: "LinkedIn",
+    address: "https://www.linkedin.com/in/bartek-d%C4%99biec-819333261/",
+    icon: LinkedInIcon,
+  },
+  {
+    name: "Facebook",
+    address: "https://www.facebook.com/bartekdbc",
+    icon: FacebookIcon,
+  },
+];
