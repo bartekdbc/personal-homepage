4cde99dde609a23ec8275b5623a57168f63678ff
Bartek Debiec <bartekdbc@gmail.com>
2023-01-18 13:29:36 +0800

    fetchPortfolio dispatched

--- src/features/homepage/Portfolio/index.js
+++ src/features/homepage/Portfolio/index.js
@@ -1,15 +1,30 @@
 import { Header, Icon, Section, Subheader, Title } from "./styled";
-import Tile from "./Tile";
+import Content from "./Content/PortfolioSuccess";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchPortfolio,
+  selectPortfolioList,
+  selectPortfolioState,
+} from "./portfolioSlice";
+import { useEffect } from "react";
 
 const Portfolio = () => {
+  const dispatch = useDispatch();
+  const portfolio = useSelector(selectPortfolioList);
+  const portfolioState = useSelector(selectPortfolioState);
+
+  useEffect(() => {
+    dispatch(fetchPortfolio());
+  }, [dispatch]);
+
   return (
     <Section>
       <Header>
-        <Icon></Icon>
+        <Icon />
         <Subheader>Portfolio</Subheader>
         <Title>My recent projects</Title>
       </Header>
-      <Tile></Tile>
+      <Content portfolio={portfolio} state={portfolioState} />
     </Section>
   );
 };
