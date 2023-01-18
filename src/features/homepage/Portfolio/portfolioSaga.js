42c82960b87f5577bf4860745a2b4bbe8de3fb98
Bartek Debiec <bartekdbc@gmail.com>
2023-01-18 13:28:24 +0800

    portfolioSaga created

---
+++ src/features/homepage/Portfolio/portfolioSaga.js
@@ -0,0 +1,22 @@
+import { call, delay, put, takeLatest } from "redux-saga/effects";
+import { getPortfolio } from "./getPortfolio";
+import { githubAPIUrl, username } from "./githubData";
+import {
+  fetchPortfolio,
+  fetchPortfolioError,
+  fetchPortfolioSuccess,
+} from "./portfolioSlice";
+
+function* fetchPortfolioHandler() {
+  try {
+    yield delay(1000);
+    const portfolio = yield call(getPortfolio, githubAPIUrl(username));
+    yield put(fetchPortfolioSuccess(portfolio));
+  } catch (error) {
+    yield put(fetchPortfolioError());
+  }
+}
+
+export function* portfolioSaga() {
+  yield takeLatest(fetchPortfolio.type, fetchPortfolioHandler);
+}
