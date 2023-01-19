6d7552c80db0e2d6896893364884e45eb8e676f9
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-01-19 11:13:02 +0800

    PortfolioFail component updated

--- src/features/homepage/Portfolio/Content/PortfolioFail/index.js
+++ src/features/homepage/Portfolio/Content/PortfolioFail/index.js
@@ -1,4 +1,5 @@
 import { ButtonLink } from "../../../../../common/ButtonLink";
+import { username } from "../../githubData";
 import { ErrorIcon, ErrorText, Paragraph, Wrapper } from "./styled";
 
 const PortfolioFail = () => (
@@ -9,7 +10,14 @@ const PortfolioFail = () => (
       Sorry, failed to load Github projects. You can check them directly on
       Github.
     </Paragraph>
-    <ButtonLink>Go to Github</ButtonLink>
+    <ButtonLink
+      href={`https://github.com/${username}`}
+      title="Github"
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      Go to Github
+    </ButtonLink>
   </Wrapper>
 );
 
