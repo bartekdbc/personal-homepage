a15dc8002cbf011925e1dbda441dbc5ea00424d7
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-02-21 17:20:48 +1300

    portfolioList changed to null in portfolioSlice

--- src/features/homepage/Portfolio/portfolioSlice.js
+++ src/features/homepage/Portfolio/portfolioSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const portfolioSlice = createSlice({
   name: "portfolio",
   initialState: {
-    portfolioList: [],
+    portfolioList: null,
     portfolioState: "initial",
   },
   reducers: {
